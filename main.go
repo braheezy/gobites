c3c746b16db19b7984be50cd65c6e46cbb6ac370
mbraha <michael.c.braha@gmail.com>
2022-08-03 10:22:39 -0700

    Allow main to play gobites

--- main.go
+++ main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+
+	"github.com/braheezy/gobites/pkg/_2_scrabble"
+)
+
 func main() {
+	runArg := flag.String("run", "", "Choose what to run.")
+
+	flag.Parse()
+
+	switch *runArg {
+	case "":
+		fmt.Println("No command provided. Try 'scrabble'.")
+	case "scrabble":
+		_2_scrabble.PlayScrabble()
+	default:
+		fmt.Println("Unknown command.")
+	}
 }
