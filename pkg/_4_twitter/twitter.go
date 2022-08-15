33386bccdbbb74ccc03be7a5a1f4fed5a5e768c1
mbraha <michael.c.braha@gmail.com>
2022-08-14 20:48:25 -0700

    Finish Gobite now that similarity metric is implemented

--- pkg/_4_twitter/twitter.go
+++ pkg/_4_twitter/twitter.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -19,7 +20,7 @@ import (
 
 // Globally limit historical tweet retrieval
 const YearsAgo = 0
-const MonthsAgo = 1
+const MonthsAgo = 5
 const DaysAgo = 0
 
 // Limit for any pagination operations
@@ -46,6 +47,22 @@ func NewTwitterUser(handle string, c TwitterClient) TwitterUser {
 	return TwitterUser{id, name, username, tweets}
 }
 
+func ComputeSimilarity(tweets1 []Tweet, tweets2 []Tweet) float64 {
+	var document1 []string
+	for _, tweet := range tweets1 {
+		document1 = append(document1, tweet.Text)
+	}
+	doc1 := strings.Join(document1, " ")
+
+	var document2 []string
+	for _, tweet := range tweets2 {
+		document2 = append(document2, tweet.Text)
+	}
+	doc2 := strings.Join(document2, " ")
+
+	return CosineSimilarity(doc1, doc2)
+}
+
 func Run() {
 	// Get secrets from env file
 	err := godotenv.Load()
@@ -58,12 +75,25 @@ func Run() {
 
 	// Define user handles to inspect
 	// TODO: Let user input handles to inspect
-	user1 := "elonmusk"
+	user1 := "JoeBiden"
+	user2 := "elonmusk"
 
 	// Create new TwitterUser for each user
 	TwitterUser1 := NewTwitterUser(user1, TwitterClient)
 
-	fmt.Printf("Got a total of %v tweets!\n", len(TwitterUser1.Tweets))
-	fmt.Printf("Newest tweet: %s\n", TwitterUser1.Tweets[0])
-	fmt.Printf("Oldest tweet: %s\n", TwitterUser1.Tweets[len(TwitterUser1.Tweets)-1])
+	fmt.Printf("Got a total of %v tweets for %s\n", len(TwitterUser1.Tweets), user1)
+	// fmt.Printf("Newest tweet: %s\n", TwitterUser1.Tweets[0])
+	// fmt.Printf("Oldest tweet: %s\n", TwitterUser1.Tweets[len(TwitterUser1.Tweets)-1])
+
+	TwitterUser2 := NewTwitterUser(user2, TwitterClient)
+
+	fmt.Printf("Got a total of %v tweets for %s\n", len(TwitterUser2.Tweets), user2)
+	// fmt.Printf("Newest tweet: %s\n", TwitterUser2.Tweets[0])
+	// fmt.Printf("Oldest tweet: %s\n", TwitterUser2.Tweets[len(TwitterUser2.Tweets)-1])
+
+	fmt.Printf("\nUser %s and %s have a tweet similarity score of: %0.4f\n",
+		TwitterUser1.Username,
+		TwitterUser2.Username,
+		ComputeSimilarity(TwitterUser1.Tweets, TwitterUser2.Tweets),
+	)
 }
